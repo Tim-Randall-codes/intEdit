e2ae9fb45869570dd4b7ae801b0b879406cabf03
Tim Randall <trtimrandall@gmail.com>
2021-06-14 00:07:46 +1000

    Initial Commit

---
+++ int/intApp.swift
@@ -0,0 +1,17 @@
+//
+//  intApp.swift
+//  int
+//
+//  Created by Tim Randall on 14/6/21.
+//
+
+import SwiftUI
+
+@main
+struct intApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
